cffcac26f0af29f114791ea9eeed84d2fd486c70
Kevan RWA Dev <kevan@rwa-hq.dev>
2025-09-22 08:12:18 -0400

    Update HTML landing page with simulated contract addresses from Amoy deployment script

--- script/00_DeployAll.s.sol
+++ script/00_DeployAll.s.sol
@@ -18,30 +18,30 @@ contract DeployAll is Script {
         vm.startBroadcast();
 
         uint256 maxSupply = 1_000_000_000 * 1e18;
+        address deployer = msg.sender;
 
         // Deploy contracts
         ReferralRegistry referrals = new ReferralRegistry();
         BooCostumeNFT nft = new BooCostumeNFT("https://ipfs.io/ipfs/QmYourIPFSHash"); // update with real IPFS
         BooPoints points = new BooPoints(address(nft));
         BooToken token = new BooToken(maxSupply);
-        LaunchGuard guard = new LaunchGuard(address(this), 5, 1000 * 1e18, 10000 * 1e18, block.timestamp + 10 minutes);
+        
+        // Allocate tokens after launch (can't transfer when paused)
+        // uint256 airdropAlloc = maxSupply * 10 / 100; // 10%
+        // uint256 lpAlloc = maxSupply * 10 / 100; // 10%
+        AirdropMerkle airdrop = new AirdropMerkle(address(token), bytes32(0)); // placeholder root
         LPVault lpVault = new LPVault(address(nft)); // Use BooCostumeNFT as placeholder ERC721
+        // require(token.transfer(address(airdrop), airdropAlloc)); // do after unpause
+        // require(token.transfer(address(lpVault), lpAlloc)); // do after unpause
+        
+        LaunchGuard guard = new LaunchGuard(deployer, 5, 1000 * 1e18, 10000 * 1e18, block.timestamp + 10 minutes);
         FeeRouter feeRouter = new FeeRouter(address(token), address(lpVault), address(points), address(referrals)); // prize to points, affiliate to referrals
         StakingManager staking = new StakingManager(address(token));
-        AirdropMerkle airdrop = new AirdropMerkle(address(token), bytes32(0)); // placeholder root
 
         // Wire them
         token.setReferrals(address(referrals));
         token.setLaunchGuard(address(guard));
         // token.setFeeRouter(address(feeRouter)); // if enabling fees
-
-        // Allocate tokens
-        uint256 airdropAlloc = maxSupply * 10 / 100; // 10%
-        uint256 lpAlloc = maxSupply * 10 / 100; // 10%
-        // Transfer to multisigs (placeholders)
-        require(token.transfer(address(airdrop), airdropAlloc));
-        require(token.transfer(address(lpVault), lpAlloc));
-        // Keep rest in deployer
 
         vm.stopBroadcast();
 
